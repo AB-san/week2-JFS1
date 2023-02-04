8aa8ae36210239f01168ea47b4e966604d62ce2d
Ansuman Behera <sonuansuman833@gmail.com>
2023-02-04 19:48:25 +0530

    Almighty Push

---
+++ prog2.java
@@ -0,0 +1,17 @@
+class App {
+    App() {
+        System.out.println("Constructor of App");
+    }
+    App(int num) {
+        System.out.println(num);
+    }
+}
+public class prog2 extends App{
+    prog2() {
+        super(20); //used to call constructor of base class
+        System.out.println("Constructor of Test2");
+    }
+    public static void main(String[] args) {
+        new prog2();
+    }
+}
